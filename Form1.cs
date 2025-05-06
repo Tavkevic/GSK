7933b11bf9fe8b72f373126153087d5ae4c5e6d6
artem <tavkevicwork@gmail.com>
2025-05-07 00:00:20 +0400

    dell ориентируемый метод закрашивания | 06.05.2025 23:59

--- Form1.cs
+++ Form1.cs
@@ -90,14 +90,8 @@ namespace Lab_2_Polygons
                     // Замыкаем фигуру, соединяя третью точку с первой
                     g.DrawLine(dashedPen, VertexList[2], VertexList[0]);
 
-                    if (fillStyleComboBox.SelectedIndex == 0)
-                    {
-                        FillPolygonStandard();
-                    }
-                    else if (fillStyleComboBox.SelectedIndex == 1)
-                    {
-                        FillPolygonOriented();
-                    }
+                    FillPolygonStandard();
+                    
                     VertexList.Clear();
                     isDrawing = false;
                 }
@@ -106,14 +100,8 @@ namespace Lab_2_Polygons
                     // Замыкаем фигуру, соединяя последнюю точку с первой
                     g.DrawLine(DrawPen, VertexList[VertexList.Count - 1], VertexList[0]);
 
-                    if (fillStyleComboBox.SelectedIndex == 0)
-                    {
-                        FillPolygonStandard();
-                    }
-                    else if (fillStyleComboBox.SelectedIndex == 1)
-                    {
-                        FillPolygonOriented();
-                    }
+                    FillPolygonStandard();
+                    
 
                     // Очищаем список вершин и завершаем процесс рисования
                     VertexList.Clear();
@@ -215,161 +203,6 @@ namespace Lab_2_Polygons
                     int xr = Xb[j + 1];
                     g.DrawLine(new Pen(colorPickButton.BackColor), xl, Y, xr, Y);
                 }
-            }
-        }
-
-        // Ориентированный алгоритм закрашивания
-        private void FillPolygonOriented()
-        {
-            // Проверяем, что в списке достаточно вершин
-            if (VertexList.Count < 3)
-            {
-                MessageBox.Show("Для закрашивания нужно как минимум 3 вершины.");
-                return;
-            }
-
-            // Определяем направление обхода (по часовой стрелке или против)
-            bool isClockwise = IsPolygonClockwise();
-
-            // Находим минимальную и максимальную координаты Y
-            int yMin = VertexList.Min(p => p.Y);
-            int yMax = VertexList.Max(p => p.Y);
-            
-
-            // Если обход по часовой стрелке, закрашиваем внешнюю область
-            if (isClockwise)
-            {
-                FillExternalRegion(0, pictureBox.Height);
-            }
-            
-            else // Иначе закрашиваем внутреннюю область
-            {
-                FillInternalRegion(yMin, yMax);
-            }
-        }
-
-        // Закрашивание внешней области
-        private void FillExternalRegion(int yMin, int yMax)
-        {
-            for (int y = yMin; y <= yMax; y++)
-            {
-                List<int> xLeft = new List<int>(); // Левые границы
-                List<int> xRight = new List<int>(); // Правые границы
-
-                // Находим пересечения сторон с текущей строкой Y
-                for (int i = 0; i < VertexList.Count; i++)
-                {
-                    int nextIndex = (i + 1) % VertexList.Count;
-
-                    int yi = VertexList[i].Y;
-                    int yk = VertexList[nextIndex].Y;
-
-                    if ((yi < y && yk >= y) || (yi >= y && yk < y))
-                    {
-                        double x = CalculateIntersectionX(VertexList[i], VertexList[nextIndex], y);
-                        if (yk - yi > 0) // Сторона направлена вверх
-                        {
-                            xRight.Add((int)Math.Round(x));
-                        }
-                        else // Сторона направлена вниз
-                        {
-                            xLeft.Add((int)Math.Round(x));
-                        }
-                    }
-                }
-
-                // Добавляем границы области вывода, если нужно
-                if (xLeft.Count == 0 && xRight.Count == 0)
-                {
-                    // Если пересечений нет, закрашиваем всю строку
-                    g.DrawLine(new Pen(colorPickButton.BackColor), 0, y, pictureBox.Width, y);
-                }
-                else
-                {
-                    // Сортируем списки границ
-                    xLeft.Sort();
-                    xRight.Sort();
-
-                    // Добавляем левую и правую границы области вывода
-                    xLeft.Insert(0, 0); // Левая граница
-                    xRight.Add(pictureBox.Width); // Правая граница
-
-                    // Закрашиваем сегменты
-                    for (int j = 0; j < xLeft.Count; j++)
-                    {
-                        int xl = xLeft[j];
-                        int xr = xRight[j];
-                        if (xl < xr) // Проверка на корректность сегмента
-                        {
-                            g.DrawLine(new Pen(colorPickButton.BackColor), xl, y, xr, y);
-                        }
-                    }
-                }
-            }
-        }
-
-        // Закрашивание внутренней области
-        private void FillInternalRegion(int yMin, int yMax)
-        {
-            for (int y = yMin; y <= yMax; y++)
-            {
-                List<int> xIntersections = new List<int>();
-
-                // Находим пересечения сторон с текущей строкой Y
-                for (int i = 0; i < VertexList.Count; i++)
-                {
-                    int nextIndex = (i + 1) % VertexList.Count;
-
-                    int yi = VertexList[i].Y;
-                    int yk = VertexList[nextIndex].Y;
-
-                    if ((yi < y && yk >= y) || (yi >= y && yk < y))
-                    {
-                        double x = CalculateIntersectionX(VertexList[i], VertexList[nextIndex], y);
-                        xIntersections.Add((int)Math.Round(x));
-                    }
-                }
-
-                // Сортируем пересечения
-                xIntersections.Sort();
-
-                // Закрашиваем сегменты между парами пересечений
-                for (int j = 0; j < xIntersections.Count; j += 2)
-                {
-                    if (j + 1 < xIntersections.Count)
-                    {
-                        int xl = xIntersections[j];
-                        int xr = xIntersections[j + 1];
-                        g.DrawLine(new Pen(colorPickButton.BackColor), xl, y, xr, y);
-                    }
-                }
-            }
-        }
-
-        // Определяет, обходятся ли вершины по часовой стрелке
-        private bool IsPolygonClockwise()
-        {
-            double area = 0;
-            for (int i = 0; i < VertexList.Count; i++)
-            {
-                int nextIndex = (i + 1) % VertexList.Count;
-                area += (VertexList[nextIndex].X - VertexList[i].X) * (VertexList[nextIndex].Y + VertexList[i].Y);
-            }
-            return area > 0; // Если площадь положительная, обход по часовой стрелке
-        }
-
-        // Вычисляет координату X пересечения стороны с горизонтальной линией Y
-        private int CalculateIntersectionX(Point p1, Point p2, int y)
-        {
-            return p1.X + (p2.X - p1.X) * (y - p1.Y) / (p2.Y - p1.Y);
-        }
-
-        // Закрашивает горизонтальную область между yStart и yEnd
-        private void FillHorizontalRegion(int yStart, int yEnd)
-        {
-            for (int y = yStart; y <= yEnd; y++)
-            {
-                g.DrawLine(new Pen(colorPickButton.BackColor), 0, y, pictureBox.Width, y);
             }
         }
     }
